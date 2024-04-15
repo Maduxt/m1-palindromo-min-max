1e778dcbb788dcdb19d14c6870406d4563f926ec
maduxt <maria.eduarda2812@hotmail.com>
2024-04-15 14:32:30 -0300

    Entrega de tarefa

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 function isPalindrome(str){
-    /* Seu código aqui */
+    str = str.toLowerCase().replaceAll(' ', '')
+    let reverseFrase = ''
+
+    for (let i = str.length -1; i >= 0; i--) {
+        reverseFrase += str[i]
+    }
+    
+    if (str === reverseFrase) {
+        return true
+    } else {
+        return false
+    }
 }
 
 function arrayMaxMin(arr){
-    /* Seu código aqui */
+    let min = Number.MAX_VALUE
+    let max = Number.MIN_VALUE
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i]
+        } 
+        if (arr[i] > max) {
+            max = arr[i]
+        }
+    }
+    const result = [min, max]
+    return result
 }
